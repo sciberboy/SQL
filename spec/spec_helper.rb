573ce0a11a0963b42b5e33849ab2280606d80b3a
Stephan <sciberboy@gmail.com>
2022-08-22 20:18:12 +0200

    Add config file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,3 @@ require 'minitest/autorun'
 
 $LOAD_PATH.unshift(%(#{__dir__}/../config))
 require 'config'
-
-# rubocop:disable Style/MixinUsage
-include Exceptions
-# rubocop:enable Style/MixinUsage
