672398283bac1cac6e51db67ca5e3ace84db3b15
Stephan <sciberboy@gmail.com>
2022-08-24 19:56:44 +0200

    Remove empty line

--- lib/sql_orm.rb
+++ lib/sql_orm.rb
@@ -7,7 +7,6 @@ DB = Sequel.sqlite('db/database.sqlite3')
 unless DB.table_exists?(:fruits)
   DB.create_table :fruits do
     primary_key :id
-
     column :name, String
     column :amount, Integer
   end
